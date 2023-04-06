5af4a779d97c0fa9b3ba81343439bb348f84fd83
davekpatrick <davekpatrick@icloud.com>
2023-04-05 21:48:45 -0400

    cleanup

--- node/index.js
+++ node/index.js
@@ -2,54 +2,53 @@
 // ------------------------------------
 // External modules
 // ------------------------------------
-const core   = require('@actions/core');   // Microsoft's actions toolkit
-const github = require('@actions/github'); // Microsoft's actions github toolkit
-const semver = require('semver');          // Node's semver package
+const core = require('@actions/core') // Microsoft's actions toolkit
+const semver = require('semver') // Node's semver package
 // ------------------------------------
 // Internal modules
 // ------------------------------------
-const getVersion = require('./lib/get-version');
+const getVersion = require('./lib/get-version')
 //
-(async () => {
+;(async () => {
   try {
     // Remember that inputs are defined in action metadata file
-    const argTagPrefix = core.getInput('tagPrefix');
-    const argApiToken  = core.getInput('apiToken');
-    const envApiToken  = process.env.GITHUB_TOKEN;  // doc: https://nodejs.org/dist/latest-v8.x/docs/api/process.html
-    // 
-    core.debug(`tagPrefix[${argTagPrefix}]`);
+    const argTagPrefix = core.getInput('tagPrefix')
+    const argApiToken = core.getInput('apiToken')
+    const envApiToken = process.env.GITHUB_TOKEN // doc: https://nodejs.org/dist/latest-v8.x/docs/api/process.html
+    //
+    core.debug(`tagPrefix[${argTagPrefix}]`)
     // Ensure we have a usable API token
-    var apiToken = null;
-    if ( argApiToken !== null && argApiToken !== '' ) {
-      core.debug('API token input provided');
-      apiToken = argApiToken;
-    } else if ( envApiToken !== null && envApiToken !== '' ) {
-      core.debug('Environment API token found');
-      apiToken = envApiToken;
+    var apiToken = null
+    if (argApiToken !== null && argApiToken !== '') {
+      core.debug('API token input provided')
+      apiToken = argApiToken
+    } else if (envApiToken !== null && envApiToken !== '') {
+      core.debug('Environment API token found')
+      apiToken = envApiToken
     } else {
-      core.setFailed('No API token found');
+      core.setFailed('No API token found')
     }
-    core.setSecret(apiToken); // ensure we don't log the token
-    // 
-    var currentVersion = await getVersion(apiToken);
-    core.info(`currentVersion[${currentVersion}]`);
+    core.setSecret(apiToken) // ensure we don't log the token
+    //
+    var currentVersion = await getVersion(apiToken)
+    core.info(`currentVersion[${currentVersion}]`)
     // increment the version
-    var versionTag = null;
-    if ( currentVersion === null ) {
+    var versionTag = null
+    if (currentVersion === null) {
       // no current version, so start at 0.0.0
-      versionTag = semver.inc('0.0.0', 'minor');
+      versionTag = semver.inc('0.0.0', 'minor')
     } else {
       // increment the current version
-      versionTag = semver.inc(currentVersion, 'minor'); 
+      versionTag = semver.inc(currentVersion, 'minor')
     }
 
-    core.info(`version[${versionTag}]`);
+    core.info(`version[${versionTag}]`)
     // remember output is defined in action metadata file
-    core.setOutput("versionTag", `${versionTag}`);
+    core.setOutput('versionTag', `${versionTag}`)
   } catch (error) {
     // Should any error occur, the action will fail and the workflow will stop
     // Using the actions toolkit (core) package to log a message and set exit code
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 })()
 // EOF
--- node/lib/get-version.js
+++ node/lib/get-version.js
@@ -2,16 +2,16 @@
 // ------------------------------------
 // External modules
 // ------------------------------------
-const core   = require('@actions/core');                // Microsoft's actions toolkit
-const github = require('@actions/github');              // Microsoft's actions github toolkit
+const core = require('@actions/core') // Microsoft's actions toolkit
+const github = require('@actions/github') // Microsoft's actions github toolkit
 //
-const semverClean = require('semver/functions/clean');  // Node's semver package
+const semverClean = require('semver/functions/clean') // Node's semver package
 // ------------------------------------
 //
 // ------------------------------------
 module.exports = async function getVersion(apiToken) {
   // ------------------------------------
-  core.debug('Start getVersion');
+  core.debug('Start getVersion')
   var versionTag = null
   // doc: https://github.com/actions/toolkit/blob/main/packages/github/README.md
   //      https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types#event-object-common-properties
@@ -19,50 +19,65 @@ module.exports = async function getVersion(apiToken) {
   // https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts
   // https://docs.github.com/en/actions/learn-github-actions/variables
   // env.GITHUB_EVENT_NAME
-  core.info('contextType[' + github.context.eventName + ']');
+  core.info('contextType[' + github.context.eventName + ']')
   //
   // need to remove the secrets from the context
   //
-  core.debug('context[' + JSON.stringify(github.context) + ']');
+  core.debug('context[' + JSON.stringify(github.context) + ']')
   // setup authenticated github client
   // doc: https://github.com/actions/toolkit/blob/main/packages/github/README.md
   //      https://octokit.github.io/rest.js/v18#authentication
-  const octokit = github.getOctokit(apiToken);
+  const octokit = github.getOctokit(apiToken)
   // ------------------------------------
-  if ( github.context.eventName === 'release') {
-    // doc: https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types#releaseevent   
+  if (github.context.eventName === 'release') {
+    // doc: https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types#releaseevent
     let tagData = github.context.payload.release.tag_name
-    core.info('Release event detected, with tag[' + tagData + ']');
+    core.info('Release event detected, with tag[' + tagData + ']')
     // ensure we have a valid semver tag
-    let tagSemVer = semverClean(tagData);
-    if ( tagSemVer === null ) {
-      core.setFailed('Invalid semver tag[' + tagData + ']');
+    let tagSemVer = semverClean(tagData)
+    if (tagSemVer === null) {
+      core.setFailed('Invalid semver tag[' + tagData + ']')
     }
-  } else if ( github.context.eventName === 'push') {
+  } else if (github.context.eventName === 'push') {
     // doc: https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types#pushevent
-    let gitRef = github.context.ref;
-    let gitSha = github.context.sha;
-    let gitBeforeCommitSha = github.context.payload.before; // sha of the commit before the push
-    core.info('Push event detected, with ref[' + gitRef + '] commitsha[' + gitSha + ']');
-    core.info('beforeCommitsha[' + gitBeforeCommitSha + ']');
+    let gitRef = github.context.ref
+    let gitSha = github.context.sha
+    let gitBeforeCommitSha = github.context.payload.before // sha of the commit before the push
+    core.info(
+      'Push event detected, with ref[' + gitRef + '] commitsha[' + gitSha + ']'
+    )
+    core.info('beforeCommitsha[' + gitBeforeCommitSha + ']')
     // get the commit data before the push
-    let gitOwner = github.context.payload.repository.owner.name;
-    let gitRepo = github.context.payload.repository.name;
-    core.debug('gitOwner[' + gitOwner + '] gitRepo[' + gitRepo + ']');
+    let gitOwner = github.context.payload.repository.owner.name
+    let gitRepo = github.context.payload.repository.name
+    core.debug('gitOwner[' + gitOwner + '] gitRepo[' + gitRepo + ']')
     // https://docs.github.com/en/rest/git/commits?apiVersion=2022-11-28#get-a-commit
     let gitBeforeCommitShaData = await octokit.rest.git.getCommit({
       owner: gitOwner,
       repo: gitRepo,
       commit_sha: gitBeforeCommitSha, // sha of the commit before the push
-    });
-    core.debug('gitBeforeCommitShaData[' + JSON.stringify(gitBeforeCommitShaData) + ']');
-    // get all branches where the given commit SHA latest commit
-    let getBeforeCommitBranches = await octokit.request('GET /repos/' + gitOwner + '/' + gitRepo + '/commits/' + gitBeforeCommitSha + '/branches-where-head', {
-      owner: gitOwner,
-      repo: gitRepo,
-      commit_sha: gitBeforeCommitSha
     })
-    core.info('getBeforeCommitBranches[' + JSON.stringify(getBeforeCommitBranches) + ']');
+    core.debug(
+      'gitBeforeCommitShaData[' + JSON.stringify(gitBeforeCommitShaData) + ']'
+    )
+    // get all branches where the given commit SHA latest commit
+    let getBeforeCommitBranches = await octokit.request(
+      'GET /repos/' +
+        gitOwner +
+        '/' +
+        gitRepo +
+        '/commits/' +
+        gitBeforeCommitSha +
+        '/branches-where-head',
+      {
+        owner: gitOwner,
+        repo: gitRepo,
+        commit_sha: gitBeforeCommitSha,
+      }
+    )
+    core.info(
+      'getBeforeCommitBranches[' + JSON.stringify(getBeforeCommitBranches) + ']'
+    )
     //let tagData = github.context.payload.ref;
     // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-tags
     //let gitTagData = await octokit.rest.repos.listTags({
@@ -70,48 +85,50 @@ module.exports = async function getVersion(apiToken) {
     //  repo: gitRepo,
     //});
     //core.info('gitTagData[' + JSON.stringify(gitTagData) + ']');
-   
+
     // lets all matching refs (tags)
     // https://docs.github.com/en/rest/reference/git#list-matching-references
     let matchingTags = await octokit.rest.git.listMatchingRefs({
       owner: gitOwner,
       repo: gitRepo,
-      ref: 'tags/v'
-    });
-    core.info('matchingTags[' + JSON.stringify(matchingTags) + ']');
-    if ( matchingTags.data.length === 0 ) {
-      core.warning('No current version found');
+      ref: 'tags/v',
+    })
+    core.info('matchingTags[' + JSON.stringify(matchingTags) + ']')
+    if (matchingTags.data.length === 0) {
+      core.warning('No current version found')
       versionTag = '0.0.0'
     } else {
       // get the latest tag
-      let latestTag = matchingTags.data[0].name;
-      core.info('latestTag[' + latestTag + ']');
+      let latestTag = matchingTags.data[0].name
+      core.info('latestTag[' + latestTag + ']')
       // ensure we have a valid semver tag
-      let tagSemVer = semverClean(latestTag);
-      if ( tagSemVer === null ) {
-        core.setFailed('Invalid semver tag[' + latestTag + ']');
+      let tagSemVer = semverClean(latestTag)
+      if (tagSemVer === null) {
+        core.setFailed('Invalid semver tag[' + latestTag + ']')
       } else {
-        versionTag = tagSemVer;
+        versionTag = tagSemVer
       }
     }
- 
-  } else if ( github.context.eventName === 'pull_request') {
+  } else if (github.context.eventName === 'pull_request') {
     // doc: https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types#pullrequestevent
-    let gitRef = github.context.ref;
-    let gitSha = github.context.sha; 
-    core.info('Pull Request event detected, with ref[' + gitRef + '] commitsha[' + gitSha + ']');
+    let gitRef = github.context.ref
+    let gitSha = github.context.sha
+    core.info(
+      'Pull Request event detected, with ref[' +
+        gitRef +
+        '] commitsha[' +
+        gitSha +
+        ']'
+    )
     core.info(`context[${JSON.stringify(github.context)}}]`)
-    
   } else {
     //
-    core.info('Unknown event type[' + github.context.eventName + ']');
+    core.info('Unknown event type[' + github.context.eventName + ']')
     core.info(`context[${JSON.stringify(github.context)}}]`)
-
   }
-  
-  // ------------------------------------
-  core.debug('End getVersion');
-  return versionTag;
 
+  // ------------------------------------
+  core.debug('End getVersion')
+  return versionTag
 } // getVersion
 // EOF
