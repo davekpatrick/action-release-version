f0985d4bed722839ed1feab04a6e557317e0eaf4
davekpatrick <davekpatrick@icloud.com>
2026-02-24 02:39:32 +0000

    remove pre handling for now

--- node/lib/get-release-type.js
+++ node/lib/get-release-type.js
@@ -329,8 +329,9 @@ module.exports = async function getReleaseType(
       // check if the pull request is to the default branch
       if (gitBaseRef === gitDefaultBranch) {
         core.info('Pull request to default branch detected')
-        outChange = 'pre' + outChange
-        core.info('Pre-release change detected')
+        // TODO: add support for 'pre' 
+        //outChange = 'pre' + outChange
+        //core.info('Pre-release change detected')
       } else {
         core.info('Pull request to non-default branch detected')
       }
